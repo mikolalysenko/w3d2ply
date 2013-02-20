256a755328c259f4ef453b6e9e3e2ff932f49b3d
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-20 11:38:30 -0600

    added README stuff

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ w3d2ply
 Converts Command & Conquer Renegade format meshes to PLY files.
 
 
+Credits
+=======
+See each file for credits.  All code is GPL.
+
+bone.c, defs.c, loader.c, lod.c, matrix.c are (C) 2004 Jonathan Wilson.
+bittype.h is (C) 2002-2004 Jonathan Wilson and is based on information provided by Westwood Studios.
+w3d_file.h is (C) Westwood Studios
+w3dconvert.c (C) 2013 Mikola Lysenko
--- w3dconvert.c
+++ w3dconvert.c
@@ -2,22 +2,28 @@
 #include "w3d.h"
 
 
-/*
-Function to find a mesh given its name
-inputs: mesh name to find
-outputs: mesh
-*/
-W3dMeshStruct *FindMesh(char *MeshName)
-{
-	uint32 Count;
-	for (Count=0;Count<MeshCount;Count++)
-	{
-		if (strcmp(MeshName,Mesh[Count].Header.MeshName) == 0)
-		{
-			return &Mesh[Count];
-		}
-	}
-	return NULL;
+void convertMesh(W3dMeshStruct *mesh, FILE* fout) {
+
+  //Write PLY header
+  fprintf(fout,
+    "ply\n"
+    "format ascii 1.0\n"
+    "comment w3d2ply converted %s\n"
+    "element vertex %d\n"
+    "property float x\n"
+    "property float y\n"
+    "property float z\n"
+    "property float nx\n"
+    "property float ny\n"
+    "property float nz\n"
+    "property list uchar float influences\n"
+    "element faces %d\n"
+    "property list uchar int vertex_index\n",
+    
+      mesh->Header.MeshName,
+      mesh->Header.
+  );
+
 }
 
 /*
@@ -411,6 +417,16 @@ void DisplayMesh(char *MeshName,float32 Transform[16],uint32 HierarchyNo)
 */
 
 
+char * concatenate(const char* a, const char* b) {
+  int a_len = strlen(a), b_len = strlen(b);
+  int buf_size = a_len + b_len + 1;
+  char* result = (char*)malloc(buf_size);
+  memcpy(result, a, a_len);
+  memcpy(result+a_len, b, b_len);
+  result[a_len+b_len] = 0;
+  return result;
+}
+
 int main(int argc, char* argv[]) {
 	Initdefs();
 	if(fseek(stdin,0,SEEK_END)) {
@@ -420,5 +436,23 @@ int main(int argc, char* argv[]) {
 	size_t input_size = ftell(stdin);
   fseek(stdin,0,SEEK_SET);
 	LoadW3d(stdin, input_size);
+  const char *prefix;
+  if(argc >= 2) {
+    prefix = argv[1];
+  } else {
+    prefix = "./";
+  }
+  for (int i=0;i<MeshCount;++i) {
+    char* filename = concatenate(prefix, Mesh[Count].Header.MeshName);
+    FILE* fout = fopen(filename, "w");
+    if(!fout) {
+      fprintf(stderr, "Error creating file %s\n", filename);
+      free(filename);
+      continue;
+    }
+    free(filename);
+    convertMesh(&Mesh[i], fout);
+    fclose(fout);
+	}
   return 0;
 }
